89dcb1c4e07e4241e426a883da9668c455e926e1
MR-Sauqre <20co53@aiktc.ac.in>
2022-10-24 23:13:05 +0530

    001 - hello world

---
+++ 001_hello.py
@@ -0,0 +1,39 @@
+# First Program of Python
+
+# In this program we will see the basic of python.Let's star it.
+
+#print() is used to print anything on screen.
+print("WELCOME TO MY CHANNEL")
+
+# Now we will declare a variable.
+a = 10
+# Here a is the name of our variable. We don't need to give the datatype of
+# variable.
+
+# Let's print its value on screen.
+print("a = ",a)
+
+# To check the datatype of any variable we use type() function.
+print("Type of a = ",type(a))
+
+# To check the address of any variable we use id() function.
+print("Address of a = ",id(a))
+
+# Let's see another examples.
+b = 12.5
+print("b = ",b)
+print("Type of b = ",type(b))
+print("Address of b = ",id(b))
+
+c = "MR SQUARE"
+print("c = ",c)
+print("Type of c = ",type(c))
+print("Address of c = ",id(c))
+
+'''
+NOTES :
+1. # is used for single line comment.
+2. Triple single/double quotes are used for multi line comments.
+'''
+
+    
