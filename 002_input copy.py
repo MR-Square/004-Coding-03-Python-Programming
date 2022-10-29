28ddad3d5891c0aa71af5bf524f59ff4649f71fe
MR-Sauqre <20co53@aiktc.ac.in>
2022-10-29 22:36:04 +0530

    002 file updated

---
+++ 002_input copy.py
@@ -0,0 +1,33 @@
+'''
+TITLE : INPUT IN PYTHON
+OWMER : MOHD RAZA MOHD RAFIQUE SHAIKH
+DATE  : 25/10/2022
+'''
+# In this program we will take the input from user.
+
+# To take the input from user we use input() function.
+no1 = input("Enter a integer:")
+# NOTE: Data type of all input is alway string but we can convert it.
+
+print(no1,"is of ",type(no1),"type")
+
+# Let's convert its type
+# To convert its type into integer we will use int() function.
+no1 = int(input("Enter that number again:"))
+print("After using int() function your input is of ",type(no1),"type")
+
+# Similarly let's see more examples
+no2 = input("Enter a floating number:")
+print(no2,"is of ",type(no2),"type")
+no2 = float(input("Enter that number again:"))
+print("After using float() function your input is of ",type(no2),"type")
+
+# In python there is no character data type. A character is also string 
+# of length one.
+
+
+'''
+ABOUT MY YOUTUBE CHANNEL
+NAME : MR SQUARE
+LINK : https://www.youtube.com/channel/UCFQ-C2iL9J9cbE0X1fawH_w
+'''
